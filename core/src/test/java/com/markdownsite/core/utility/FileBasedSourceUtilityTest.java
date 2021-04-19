2379a914c480988e832f424237df1aa34daa5d3e
Vaibhav Lokhande <vaibhavglokhande@gmail.com>
2021-04-19 22:02:47 +0530

    Remove unnecessary test

--- core/src/test/java/com/markdownsite/core/utility/FileBasedSourceUtilityTest.java
+++ core/src/test/java/com/markdownsite/core/utility/FileBasedSourceUtilityTest.java
@@ -1,7 +1,5 @@
 package com.markdownsite.core.utility;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.markdownsite.core.FileBasedMarkdownSource;
 import com.markdownsite.core.FileNode;
 import com.markdownsite.integration.models.SimpleTree;
 import org.junit.jupiter.api.Test;
@@ -9,13 +7,9 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class FileBasedSourceUtilityTest {
 
@@ -28,17 +22,4 @@ class FileBasedSourceUtilityTest {
         assertEquals(2, simpleTree.getChildren(simpleTree.getRootNode()).size());
     }
 
-    @Test
-    void name() {
-        FileBasedSourceUtilityTest fileBasedSourceUtilityTest = new FileBasedSourceUtilityTest();
-        String func = fileBasedSourceUtilityTest.func(fileNode -> fileNode.getIdentifier() != null);
-        System.out.println(func);
-    }
-
-    private String func(Predicate<FileNode> fileNodePredicate){
-        boolean test = fileNodePredicate.test(new FileNode());
-        if(test)
-        return "Yes";
-        return "No";
-    }
 }
