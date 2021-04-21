2fb346c70728aa60472a5524da07eb124d147d8a
Vaibhav Lokhande <vaibhavglokhande@gmail.com>
2021-04-21 14:11:37 +0530

    Update FileBasedMarkdownSource
    Implemented NavigableMarkdownSource, added the navigation tree.

--- core/src/main/java/com/markdownsite/core/FileBasedMarkdownSource.java
+++ core/src/main/java/com/markdownsite/core/FileBasedMarkdownSource.java
@@ -5,8 +5,9 @@ import com.markdownsite.core.utility.FileBasedSourceUtility;
 import com.markdownsite.integration.enums.PropertyValidationErrorCode;
 import com.markdownsite.integration.exceptions.PropertyValidationException;
 import com.markdownsite.integration.exceptions.TreeOperationException;
-import com.markdownsite.integration.interfaces.MarkdownSource;
+import com.markdownsite.integration.interfaces.NavigableMarkdownSource;
 import com.markdownsite.integration.interfaces.SimpleTraverseMode;
+import com.markdownsite.integration.interfaces.Tree;
 import com.markdownsite.integration.models.*;
 import org.springframework.stereotype.Component;
 
@@ -22,7 +23,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Component
-public class FileBasedMarkdownSource implements MarkdownSource<String, String> {
+public class FileBasedMarkdownSource implements NavigableMarkdownSource<String, String> {
 
     public static final String ALLOWED_SOURCE_EXTENSION = ".md";
     private SimpleTree<FileNode, File> fileSimpleTree;
@@ -32,6 +33,7 @@ public class FileBasedMarkdownSource implements MarkdownSource<String, String> {
     public static final String PROPERTY_SOURCE_DIR = "sourceDirectory";
     private Map<String, SourceProviderConfigProperty<String>> sourceProviderConfig = new ConcurrentHashMap<>();
     private Map<String, MarkdownElement<String>> markdownSourceMap = new ConcurrentHashMap<>();
+    private Tree<SourceNavigationNode<String>, String> navigationTree;
 
 
     @Override
@@ -42,6 +44,19 @@ public class FileBasedMarkdownSource implements MarkdownSource<String, String> {
         FileBasedSourceUtility fileBasedSourceUtility = new FileBasedSourceUtility();
         this.fileSimpleTree = fileBasedSourceUtility.buildTree(sourceDirectoryProperty.getPropertyValue(), ALLOWED_SOURCE_EXTENSION);
         buildSource();
+        buildNavigationTree();
+    }
+
+    private void buildNavigationTree() throws TreeOperationException {
+        navigationTree = fileSimpleTree.convert(fileNode -> {
+            SourceNavigationNode<String> navigationNode = new SourceNavigationNode<>();
+            navigationNode.setDirectory(fileNode.getValue().isDirectory());
+            String identifier = getIdentifier(fileNode);
+            navigationNode.setValue(identifier);
+            navigationNode.setDisplayName(fileNode.getValue().getName());
+            navigationNode.setMarkdownElement(getMarkdownElement(identifier));
+            return navigationNode;
+        });
     }
 
     private void buildSource() throws TreeOperationException {
@@ -49,7 +64,7 @@ public class FileBasedMarkdownSource implements MarkdownSource<String, String> {
         for (FileNode fileNode : fileNodes) {
             MarkdownElement<String> markdownElement = new MarkdownElement<>();
             markdownElement.setTitle(fileNode.getValue().getName());
-            String identifier = UUID.nameUUIDFromBytes(fileNode.getValue().getAbsolutePath().getBytes(StandardCharsets.UTF_8)).toString();
+            String identifier = getIdentifier(fileNode);
             markdownElement.setIdentifier(identifier);
             File value = fileNode.getValue();
             try (Stream<String> lines = Files.lines(value.toPath())) {
@@ -61,6 +76,10 @@ public class FileBasedMarkdownSource implements MarkdownSource<String, String> {
             }
             markdownSourceMap.put(identifier, markdownElement);
         }
+    }
+
+    private String getIdentifier(FileNode fileNode) {
+        return UUID.nameUUIDFromBytes(fileNode.getValue().getAbsolutePath().getBytes(StandardCharsets.UTF_8)).toString();
     }
 
     @Override
@@ -102,5 +121,10 @@ public class FileBasedMarkdownSource implements MarkdownSource<String, String> {
                 throw new PropertyValidationException("Property value for " + sourceProviderConfigProperty.getPropertyName() + " is invalid", PropertyValidationErrorCode.INVALID_PROPERTY);
 
         }
+    }
+
+    @Override
+    public Tree<SourceNavigationNode<String>, String> getNavigationTree() {
+        return navigationTree;
     }
 }
--- integration/src/main/java/com/markdownsite/integration/interfaces/NavigableMarkdownSource.java
+++ integration/src/main/java/com/markdownsite/integration/interfaces/NavigableMarkdownSource.java
@@ -1,11 +1,21 @@
 package com.markdownsite.integration.interfaces;
 
+import com.markdownsite.integration.models.MarkdownElement;
+import com.markdownsite.integration.models.SourceNavigationNode;
+
 /**
  * The interface Navigable markdown source.
  * The navigable markdown source provides the markdown sources which can be navigated.
  * It provides a hierarchical structure to navigate through different {@link com.markdownsite.integration.models.MarkdownElement}
  *
- * @param <T> the type parameter
+ * @param <T> the type parameter for the source content type
+ * @param <G> the type parameter for the source identifier type
  */
 public interface NavigableMarkdownSource<T, G> extends MarkdownSource<T, G> {
+    /**
+     * Provides the tree to build the navigation tree.
+     *
+     * @return the navigation tree
+     */
+    Tree<SourceNavigationNode<T>, String> getNavigationTree();
 }
