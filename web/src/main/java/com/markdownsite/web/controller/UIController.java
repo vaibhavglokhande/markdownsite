4b43c8e62d37dfcfadfea77076b5af2086f47566
Vaibhav Lokhande <vaibhavglokhande@gmail.com>
2021-04-22 21:28:51 +0530

    Initial integration
    Integrated the UI service with the UI to render the markdown.

--- web/src/main/java/com/markdownsite/web/controller/UIController.java
+++ web/src/main/java/com/markdownsite/web/controller/UIController.java
@@ -3,10 +3,13 @@ package com.markdownsite.web.controller;
 import com.markdownsite.core.FileBasedMarkdownSource;
 import com.markdownsite.core.RenderEngineFactory;
 import com.markdownsite.core.interfaces.ResourceProvider;
+import com.markdownsite.core.interfaces.UIService;
 import com.markdownsite.integration.exceptions.AbstractException;
 import com.markdownsite.integration.interfaces.MarkdownSource;
 import com.markdownsite.integration.interfaces.RenderEngine;
+import com.markdownsite.integration.interfaces.Tree;
 import com.markdownsite.integration.models.MarkdownElement;
+import com.markdownsite.integration.models.SourceNavigationNode;
 import com.markdownsite.integration.models.SourceProviderConfigProperty;
 import com.markdownsite.integration.providers.MarkdownSourceProvider;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,40 +31,25 @@ public class UIController {
     // TODO Remove this hardcoded value, use configuration.
     private String renderEngineUUID = "9971ea80-dec5-3154-af52-771b08068b97";
 
-    private RenderEngineFactory renderEngineFactory;
     private ResourceProvider resourceProvider;
-    private MarkdownSourceProvider markdownSourceProvider;
+    private UIService uiService;
 
 
     @Autowired
-    public UIController(RenderEngineFactory renderEngineFactory, ResourceProvider resourceProvider, MarkdownSourceProvider markdownSourceProvider) {
-        this.renderEngineFactory = renderEngineFactory;
+    public UIController(ResourceProvider resourceProvider, UIService uiService) {
         this.resourceProvider = resourceProvider;
-        this.markdownSourceProvider = markdownSourceProvider;
+        this.uiService = uiService;
     }
 
-    @GetMapping(path = "/docs/{*docId}")
+    @GetMapping(path = "/docs/{docId}")
     public String renderDoc(@PathVariable String docId, Model model) throws AbstractException {
-        RenderEngine renderEngine = renderEngineFactory.getRenderEngine(renderEngineUUID);
-        MarkdownSource source = markdownSourceProvider.getSource(FileBasedMarkdownSource.IDENTIFIER);
-        List<SourceProviderConfigProperty> sourceProviderConfig = new ArrayList<>();
-        Path path = Paths.get("core", "src", "test", "resources", "markdown-files");
-        SourceProviderConfigProperty<String> configProperty = new SourceProviderConfigProperty<>(FileBasedMarkdownSource.PROPERTY_SOURCE_DIR, path.toString());
-        sourceProviderConfig.add(configProperty);
-        source.updateSourceConfig(sourceProviderConfig);
-        source.initializeSource();
-        Map<String, MarkdownElement<String>> allSources = source.getAll();
-        StringBuilder stringBuilder = new StringBuilder();
-        for (Map.Entry<String, MarkdownElement<String>> entry : allSources.entrySet()) {
-            MarkdownElement<String> fileBasedMarkdownSourceMarkdownElement = entry.getValue();
-            stringBuilder.append(renderEngine.render(fileBasedMarkdownSourceMarkdownElement.getContent()))
-                    .append("\n");
-        }
+        Tree<SourceNavigationNode, String> navigationTree = uiService.getNavigationTree();
+        String content = uiService.getContent(docId);
         model.addAttribute("defaultCSS", resourceProvider.getCssResources());
         model.addAttribute("defaultScript", resourceProvider.getJsResources());
         model.addAttribute("inlineCSS", resourceProvider.getInlineCssResources());
         model.addAttribute("inlineScript", resourceProvider.getInlineJsResources());
-        model.addAttribute("markdownRender", stringBuilder.toString());
+        model.addAttribute("markdownRender", content);
         return "docs/markdown-render";
     }
 
